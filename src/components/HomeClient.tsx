7913e79c8d3afc7f486a043d577db15a416d3e66
Cart <81428538+cartrigger@users.noreply.github.com>
2025-05-15 16:40:10 -0400

    no more linting because linting is for losers

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -161,7 +161,7 @@ export default function HomeClient() {
               What is XRCraft?
             </h2>
             <p className="mb-4 text-white">
-              XRCraft is a VR optimized Minecraft server that's included in QuestCraft by default, 
+              XRCraft is a VR optimized Minecraft server that iss included in QuestCraft by default, 
               providing the best experience for VR players while remaining fully compatible with PC!
             </p>
             <p className="mb-6 text-white">
@@ -206,7 +206,7 @@ export default function HomeClient() {
                 { path: '/servers', name: 'Available Servers', icon: 'M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 014.5-4.5h15a4.5 4.5 0 014.5 4.5m-4.5 6.75h-15' },
                 { path: '/status', name: 'Server Status', icon: 'M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z' },
                 { path: '/socials', name: 'Our Socials', icon: 'M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.479m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z' },
-              ].map((link, idx) => (
+              ].map((link) => (
                 <li key={link.path} className="transform hover:translate-x-2 transition-transform duration-200">
                   <Link 
                     href={link.path} 
